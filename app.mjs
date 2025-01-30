faeebbdc6b33c845bfd5600315838534292955d1
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-01-30 11:56:19 +0000

    Implement persistent user sessions

--- app.mjs
+++ app.mjs
@@ -1,7 +1,27 @@
 import express from 'express';
+import session from 'express-session';
+import { PrismaClient } from '@prisma/client';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import 'dotenv/config';
 
 const app = express();
+const prisma = new PrismaClient();
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days in millis.
+    },
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 1000 * 60 * 2, // 2 minutes in millis.
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
 
 app.get('/', (req, res) => {
   res.send('Hello, world');
