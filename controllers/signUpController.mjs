51ac968d8b94a371ff0cd5124f91b25b1a5dd27f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-02-05 11:52:54 +0000

    Add ability to sign up to app

--- controllers/signUpController.mjs
+++ controllers/signUpController.mjs
@@ -1,6 +1,51 @@
+import prisma from '../db/prisma.mjs';
+
 function getSignUp(req, res, next) {
-  res.render('sign-up/sign-up', { title: 'Sign Up' });
+  res.render('sign-up/sign-up', {
+    title: 'Sign Up',
+    formData: req.session.formData,
+    errors: req.session.errors,
+  });
   next();
 }
 
-export { getSignUp };
+function getSuccess(req, res, next) {
+  res.render('sign-up/success', { title: 'Sign Up Successful' });
+  next();
+}
+
+async function postSignUp(req, res, next) {
+  const { email, password, confirm_password } = req.body;
+
+  // Check user email not already used.
+  const existing = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  const errors = {};
+  if (existing) errors.email = 'That user already exists';
+  if (password !== confirm_password)
+    errors.confirm_password = 'Passwords do not match';
+  req.session.errors = { ...errors };
+  req.session.save(async (error) => {
+    if (error) next(error);
+    // Make sure all this stuff, and all redirects occur after the session has been saved.
+    if (!existing && password === confirm_password) {
+      await prisma.user.create({
+        data: {
+          email,
+          password,
+        },
+      });
+      // If successful, redirect to success page.
+      res.status(303).redirect('/sign-up/success');
+    } else {
+      // If failure, redirect to getSignUp route with form values and errors.
+      res.status(400).redirect('/sign-up');
+    }
+  });
+}
+
+export { getSignUp, getSuccess, postSignUp };
--- routers/signUpRouter.mjs
+++ routers/signUpRouter.mjs
@@ -1,8 +1,11 @@
 import * as signUpController from '../controllers/signUpController.mjs';
+import * as routeData from '../middleware/routeData.mjs';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', signUpController.getSignUp);
+router.get('/', signUpController.getSignUp, routeData.clearRouteData);
+router.post('/', routeData.storeFormData, signUpController.postSignUp);
+router.get('/success', signUpController.getSuccess, routeData.clearRouteData);
 
 export default router;
