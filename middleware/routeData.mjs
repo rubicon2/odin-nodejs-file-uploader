f77d04eed2d9ea1da99c33a29de59e7ad88a3b70
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-02-05 11:50:10 +0000

    Remove unnecessary session.save() from storePassportErrors middleware

--- middleware/routeData.mjs
+++ middleware/routeData.mjs
@@ -20,7 +20,6 @@ function storePassportErrors(req, res, next) {
   // Store as obj with form paths so can map errors to offending inputs, but also array for convenience, e.g. making a list.
   req.session.errors = { ...obj, array };
   delete req.session.messages;
-  req.session.save();
   next();
 }
 
