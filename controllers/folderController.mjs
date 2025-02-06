c435c3187ab31eb708b0fd7344146610535ec92d
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-02-06 17:21:22 +0000

    Add ability to update folder names and delete folders

--- controllers/folderController.mjs
+++ controllers/folderController.mjs
@@ -31,4 +31,33 @@ async function postNewFolder(req, res, next) {
   }
 }
 
-export { getFolder, postNewFolder };
+async function postUpdateFolder(req, res, next) {
+  try {
+    await prisma.folder.update({
+      where: {
+        id: parseInt(req.params.folderId),
+      },
+      data: {
+        name: req.body.name,
+      },
+    });
+    res.redirect(`/folder/${req.params.folderId}`);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function postDeleteFolder(req, res, next) {
+  try {
+    await prisma.folder.delete({
+      where: {
+        id: parseInt(req.params.folderId),
+      },
+    });
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { getFolder, postNewFolder, postUpdateFolder, postDeleteFolder };
--- routers/folderRouter.mjs
+++ routers/folderRouter.mjs
@@ -8,5 +8,7 @@ const router = Router();
 router.post('/new', isAuth, folderController.postNewFolder);
 router.get('/:folderId', isAuth, folderController.getFolder);
 router.post('/:folderId/new', isAuth, folderController.postNewFolder);
+router.post('/:folderId/update', isAuth, folderController.postUpdateFolder);
+router.post('/:folderId/delete', isAuth, folderController.postDeleteFolder);
 
 export default router;
