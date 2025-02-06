ed4d98331360b7b71f8e5cee52591a227fb6e61f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-02-06 12:20:30 +0000

    Implement file uploads to server folder

--- controllers/fileController.mjs
+++ controllers/fileController.mjs
@@ -1,5 +1,17 @@
-function getUpload(req, res) {
+function getUpload(req, res, next) {
   res.render('file/upload.ejs', { title: 'Upload file', user: req.user });
+  // Clear route data.
+  next();
 }
 
-export { getUpload };
+async function postUpload(req, res, next) {
+  try {
+    console.log(req.file);
+    console.log(req.body);
+    res.redirect('/file/upload');
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { getUpload, postUpload };
--- routers/fileRouter.mjs
+++ routers/fileRouter.mjs
@@ -1,9 +1,35 @@
 import * as fileController from '../controllers/fileController.mjs';
+import * as routeData from '../middleware/routeData.mjs';
 import { isAuth } from '../middleware/auth.mjs';
 import { Router } from 'express';
+import multer from 'multer';
 
 const router = Router();
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, done) => {
+      done(null, 'uploads/');
+    },
+    filename: (req, file, done) => {
+      const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      // How to get file extension without messing with file names that have dots in?
+      done(null, uniquePrefix + '-' + file.originalname);
+    },
+  }),
+});
 
-router.get('/upload', isAuth, fileController.getUpload);
+router.get(
+  '/upload',
+  isAuth,
+  fileController.getUpload,
+  routeData.clearRouteData,
+);
+router.post(
+  '/upload',
+  isAuth,
+  routeData.storeFormData,
+  upload.single('file'),
+  fileController.postUpload,
+);
 
 export default router;
